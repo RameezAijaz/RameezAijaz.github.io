b957187eb0ab559dcdb454e5e2c1dd3112c64a62
asurion-rameez <syedrameez.aijaz@asurion.com>
2019-12-31 18:30:24 -0500

    website added

---
+++ index.js
@@ -0,0 +1,11 @@
+setTimeout(function(){
+    document.getElementById('intro-note').style.borderRight = 'none';
+    document.getElementById('welcome-note').style.display = 'block';
+}, 6000);
+
+
+
+setTimeout(function(){
+    document.getElementsByClassName('social-links')[0].style.display = 'block';
+    document.getElementsByClassName('music-section')[0].style.display = 'block';
+}, 9000);
