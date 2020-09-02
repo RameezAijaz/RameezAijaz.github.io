6a7a463c67c00aec0a6f26c97b36b156a0b37627
Syed Rameez Aijaz <ramez.aijaz@gmail.com>
2020-09-02 12:11:36 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 setTimeout(function(){
     document.getElementById('intro-note').style.borderRight = 'none';
     document.getElementById('welcome-note').style.display = 'block';
-}, 3000);
+}, 2000);
 
 
 
 setTimeout(function(){
     document.getElementsByClassName('social-links')[0].style.display = 'block';
     document.getElementsByClassName('music-section')[0].style.display = 'block';
-}, 4000);
+}, 2000);
